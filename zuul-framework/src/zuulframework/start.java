84bd521d15c8b8267049d6567db99319b87a5140
SpraTtz <mathiastondering@gmail.com>
2016-09-26 10:54:51 +0200

    Add files via upload

---
+++ zuul-framework/src/zuulframework/start.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zuulframework;
+
+/**
+ *
+ * @author Mathias
+ */
+
+public class start{
+    public static void main(String[] args){
+        //make an object of the game - which we can use in the other files we are going to make
+        Game g = new Game();
+        //start the game - Here we start the game variable "g" which is our game.
+        g.play();
+    }
+}
