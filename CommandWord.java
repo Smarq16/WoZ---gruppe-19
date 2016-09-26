29c86723a47e50d485fc8d98e76c3201f3068895
SpraTtz <mathiastondering@gmail.com>
2016-09-26 10:57:11 +0200

    Add files via upload

---
+++ CommandWord.java
@@ -0,0 +1,15 @@
+package zuulframework;
+
+ public enum CommandWord{
+    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
+    
+    private String commandString;
+    
+    CommandWord(String commandString){
+        this.commandString = commandString;
+    }
+    
+    public String toString(){
+        return commandString;
+    }
+}
